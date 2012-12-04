8924650a940769cce1227fa2967107108a9746e5
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-04 07:37:19 +0100

    Use plain QApplication instead of VApplication.
    Closes #7.

--- src/main.cpp
+++ src/main.cpp
@@ -20,18 +20,17 @@
  * along with Swordfish.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************************************************/
 
-#include <VApplication>
+#include <QApplication>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    VApplication app(argc, argv);
+    QApplication app(argc, argv);
     app.setApplicationName("Swordfish");
     app.setApplicationVersion("0.0.0");
     app.setOrganizationDomain("maui-project.org");
-    app.setOrganizationName("Maui");
-    app.setIdentifier("org.maui.Swordfish");
+    app.setOrganizationName("Hawaii");
     app.setWindowIcon(QIcon::fromTheme("system-file-manager"));
 
     // Show the main window
