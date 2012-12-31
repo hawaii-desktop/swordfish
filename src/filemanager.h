2e4ac27bc18f650610ba7ffebfb21da8311c06e6
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-31 16:09:30 +0100

    Change license.
    Change license to GPL 2 or, at your option, any later version.

--- src/filemanager.h
+++ src/filemanager.h
@@ -1,23 +1,27 @@
 /****************************************************************************
- * This file is part of Swordfish
+ * This file is part of Swordfish.
  *
- * Copyright (c) 2010-2012 Pier Luigi Fiorini
+ * Copyright (C) 2010-2012 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
  * Author(s):
- *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *    Pier Luigi Fiorini
  *
- * Swordfish is free software: you can redistribute it and/or modify
+ * $BEGIN_LICENSE:GPL2+$
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * Swordfish is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Swordfish.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
  ***************************************************************************/
 
 #ifndef FILEMANAGER_H
