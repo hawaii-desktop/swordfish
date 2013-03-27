6f754ce862023a61e7d2756abe24743c03ed0ebc
jatt159 <natserver2@gmail.com>
2013-03-27 21:26:58 +0100

    Added new class Mount
    This class is needed for PlacesModel and it is based on PlacesItem
    This class uses Kommodity/GIO/Mount which is based on GMount
    Please consult: https://developer.gnome.org/gio/unstable/GMount.html

---
+++ src/mount.cpp
@@ -0,0 +1,47 @@
+/****************************************************************************
+ * This file is part of Swordfish.
+ *
+ * Copyright (C) 2010-2013 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:GPL2+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include "mount.h"
+
+Mount::Mount(const Kommodity::GIO::Mount &mount)
+: PlacesItem()
+{
+    update();
+    setEditable(true);
+}
+
+void Mount::update()
+{
+    setText(m_mount->getName());
+
+    Kommodity::GIO::File mFile = m_mount->getRoot();
+    if (mFile.getUri().isValid())
+        setUrl(mFile.getUri());
+    else
+        setUrl(QUrl::fromUserInput("unknown"));
+
+    setIcon(m_mount->getIcon()->toQIcon());
+}
