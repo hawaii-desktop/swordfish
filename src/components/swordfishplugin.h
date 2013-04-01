5500644af2c0177ea363a8d03dacf616f6f066a9
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-04-01 20:58:58 +0200

    Swordfish components plugin
    Move all the models stuff into a QML plugin and add a simple component
    to test the PlacesModel.

--- src/components/swordfishplugin.h
+++ src/components/swordfishplugin.h
@@ -6,46 +6,36 @@
  * Author(s):
  *    Pier Luigi Fiorini
  *
- * $BEGIN_LICENSE:GPL2+$
+ * $BEGIN_LICENSE:LGPL2.1+$
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * $END_LICENSE$
  ***************************************************************************/
 
-#ifndef MOUNT_H
-#define MOUNT_H
+#ifndef SWORDFISHPLUGIN_H
+#define SWORDFISHPLUGIN_H
 
-#include <Kommodity/GIO/Mount>
-#include <Kommodity/GIO/File>
+#include <QQmlExtensionPlugin>
 
-#include "placesitem.h"
-
-class Mount : public PlacesItem
+class SwordfishPlugin : public QQmlExtensionPlugin
 {
-
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
 public:
-    Mount(const Kommodity::GIO::Mount &mount);
-    void update();
-
-    Kommodity::GIO::Mount *mount() const
-    {
-        return m_mount;
-    }
-
-private:
-    Kommodity::GIO::Mount *m_mount;
+    void registerTypes(const char *uri);
+    void initializeEngine(QQmlEngine *engine, const char *uri);
 };
 
-#endif // MOUNT_H
+#endif // SWORDFISHPLUGIN_H
