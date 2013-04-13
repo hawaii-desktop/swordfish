6f8ab6948984bd6771fd77ece1867d3d1b3666f5
jatt159 <natserver2@gmail.com>
2013-04-13 16:51:52 +0200

    New functions declared Expected a better implementation

--- src/components/foldermodel.h
+++ src/components/foldermodel.h
@@ -5,6 +5,7 @@
 #include <QAbstractListModel>
 #include <QDir>
 #include <QUrl>
+#include <QModelIndex>
 #include <Kommodity/GIO/FileInfo>
 
 #include "folderitem.h"
@@ -13,7 +14,7 @@ class FolderModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
-    FolderModel();
+    FolderModel(QObject* parent = 0);
     
     enum Role {
         FileInfoRole = Qt::UserRole
@@ -38,20 +39,21 @@ public:
         return QUrl::fromUserInput(m_folder->absolutePath());
     }
 
+    void setFolder(const QDir &newFolder);
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
     int columnCount(const QModelIndex &parent) const;
 
     QVariant data(const QModelIndex &index, int role) const;
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    QVariant headerData(int section,
+                          Qt::Orientation orientation,
+                          int role) const;
 
-    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+    QModelIndex index(int row,
+                      int column,
+                      const QModelIndex &parent = QModelIndex()) const;
     QModelIndex parent(const QModelIndex &index) const;
 
-    Qt::ItemFlags flags(const QModelIndex &index) const;
-
     Kommodity::GIO::FileInfo *fileInfoFromIndex(const QModelIndex &index) const;
-
-    void setFolder(const QDir &folder);
 
 protected:
     void insertFiles(const int &row,const QList<Kommodity::GIO::FileInfo *> &fileInfoList);
@@ -60,11 +62,12 @@ protected:
     QList<FolderItem>::Iterator findItemByUrl(const QUrl &url, const int &row);
     QList<FolderItem>::Iterator findItemByName(const QString &name, const int &row);
 
-    FolderItem itemFromIndex(const QModelIndex &index) const;
+    FolderItem *itemFromIndex(const QModelIndex &index) const;
 
 private:
     QDir *m_folder;
-    QHash<FolderItem, int> m_folderItems;
+    QList<FolderItem> m_folderItems;
+    //QHash<FolderItem, int> m_folderItems;
 };
 
 #endif // FOLDERMODEL_H
