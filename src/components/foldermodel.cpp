6ae197b303617130d9b7f59de6071e1419562294
jatt159 <natserver2@gmail.com>
2013-04-15 17:33:14 +0200

    FolderModel updated Added license The base class constructor is initialized Added a method to find out the file owner

--- src/components/foldermodel.cpp
+++ src/components/foldermodel.cpp
@@ -1,6 +1,33 @@
+/****************************************************************************
+ * This file is part of Swordfish.
+ *
+ * Copyright (C) 2010-2013 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
 #include "foldermodel.h"
 
 FolderModel::FolderModel(QObject* parent)
+    : QAbstractListModel(parent)
 {
 }
 
@@ -72,7 +99,7 @@ QVariant FolderModel::data(const QModelIndex &index, int role = Qt::DisplayRole)
         }
 
         case ColumnFileOwner : {
-            //TODO: Show owner name
+            return QVariant(fileinfo->getAttributeAsString("owner::user"));
             break;
         }
         }
